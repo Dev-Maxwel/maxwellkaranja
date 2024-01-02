b24584294711bbb244ada706034306be338f2e3a
devmaxwell <developermaxwellkaranja@gmail.com>
2024-01-02 20:48:08 +0300

    second commit

--- maxwellkaranja/portfolio/urls.py
+++ maxwellkaranja/portfolio/urls.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+#if Static does not load
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# #if Static Files does not load add the above line of code imediately after the bracket for url patterns
+
 
 
 
