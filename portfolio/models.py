ab2fa3771b3542d809b8cd8cb3ea8d0a81a20923
devmaxwell <developermaxwellkaranja@gmail.com>
2024-01-06 21:19:48 +0300

    message registered to the database

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class emailingInfo(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+    phone = models.CharField(max_length=50, blank=True, null=True)
+    email = models.EmailField(max_length=70, blank=True, null=True)
+    message = models.TextField(max_length=10000, blank=True, null=True)
+    
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .models import emailingInfo
 
 def index(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+        email = request.POST.get("email")
+        # Save the emailsending details to the database
+        emailInfo = emailingInfo(name=name, phone=phone, email=email, message=message)
+        emailInfo.save()    
+    else:
+        pass           
     return render(request, 'index.html')
 
