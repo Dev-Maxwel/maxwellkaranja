23ef541bf1f882cbeba4792c61b91039de0a2265
devmaxwell <developermaxwellkaranja@gmail.com>
2024-01-06 22:05:59 +0300

    email sending tested and complete

--- maxwellkaranja/settings.py
+++ maxwellkaranja/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,3 +150,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
+#email sending
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config("EADDRESS")
+EMAIL_HOST_PASSWORD = config("EPASSWORD")
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
 from .models import emailingInfo
+from django.core.mail import send_mail
+from django.conf import settings
 
 def index(request):
+    #sending mail
     if request.method == 'POST':
         name = request.POST.get("name")
         phone = request.POST.get("phone")
         message = request.POST.get("message")
         email = request.POST.get("email")
-        # Save the emailsending details to the database
-        emailInfo = emailingInfo(name=name, phone=phone, email=email, message=message)
-        emailInfo.save()    
-    else:
-        pass           
-    return render(request, 'index.html')
+        
+        #send_mail function parameters
+        send_mail(
+            f"Message from {name}",
+            f"{message}\n\nSender's Name: {name}\nPhone: {phone}\nEmail: {email}",
+            email,  # Use the correct variable for sender's email
+            ['developermaxwellkaranja@gmail.com'],  # Use the correct variable for recipient's email
+            fail_silently=False,
+        )
 
+        # Save the email sending details to the database
+        emailInfo = emailingInfo(name=name, phone=phone, email=email, message=message)
+        emailInfo.save() 
+        return render(request, 'index.html')
+    else:
+        pass
+
+    return render(request, 'index.html')
