6a20c276aec8e9d5b3bf55a9291891ec9800d637
devmaxwell <developermaxwellkaranja@gmail.com>
2024-01-13 20:50:41 +0300

    landing page changed

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,6 +4,7 @@ class emailingInfo(models.Model):
     name = models.CharField(max_length=50, blank=True, null=True)
     phone = models.CharField(max_length=50, blank=True, null=True)
     email = models.EmailField(max_length=70, blank=True, null=True)
+    subject = models.TextField(max_length=100, blank=True, null=True)
     message = models.TextField(max_length=10000, blank=True, null=True)
     timeStamp = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     
---
+++ portfolio/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from .models import emailingInfo
+from django.core.mail import send_mail
+from django.conf import settings
+
+def home(request):
+    #sending mail
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("subject")
+        message = request.POST.get("message")
+        email = request.POST.get("email")
+        
+        #send_mail function parameters
+        send_mail(
+            f"Message from {name}",
+            f"{message}\n\nSender's Name: {name}\nPhone: {phone}\nEmail: {email}",
+            email,  #sender's email
+            ['developermaxwellkaranja@gmail.com', "mxkaranja@gmail.com"],  #recipient's email
+            fail_silently=False,
+        )
+
+        # Save the email sending details to the database
+        emailInfo = emailingInfo(name=name, phone=phone, email=email, message=message)
+        emailInfo.save() 
+        return render(request, 'index.html')
+    else:
+        pass
+
+    return render(request, 'index.html')
