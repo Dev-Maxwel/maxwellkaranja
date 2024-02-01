9a64c60d5e7d7bebd672fbfbc7f1d99ccb031877
devmaxwell <developermaxwellkaranja@gmail.com>
2024-02-01 22:22:38 +0300

    header update

--- maxwellkaranja/settings.py
+++ maxwellkaranja/settings.py
@@ -140,8 +140,6 @@ STORAGES = {
 # ##On older Django versions:
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-
-
     
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]   
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -151,8 +149,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-
 
 #email sending
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
